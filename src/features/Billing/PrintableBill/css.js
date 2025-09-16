b1d90682bb4680cc7a3a088c6ed84a789511e67c
Aadarsh Gupta <prathamtogupta11@gmail.com>
2025-09-16 21:14:00 +0530

    few more

--- src/features/Billing/PrintableBill/css.js
+++ src/features/Billing/PrintableBill/css.js
@@ -1,2 +1,2 @@
-export const srStyle = `w-[10%] border text-center p-1 md:py-2`;
-export const dataStyle = `w-[30%] border text-center p-1 md:p-2`;
+export const srStyle = `w-[10%] border text-center p-1 py-2 md:py-2`;
+export const dataStyle = `w-[30%] border text-center p-1 py-2 md:p-2`;
