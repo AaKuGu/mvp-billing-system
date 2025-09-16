8707a0545957e2da27097dbda4d77c36b0cac982
Aadarsh Gupta <prathamtogupta11@gmail.com>
2025-09-16 10:40:29 +0530

    red button

---
+++ src/features/Billing/BillingItems/BillingItems.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import { createEmptyBillData } from "./constant";
+import Header from "@/shared/components/ui/Header";
+import { fetchProducts } from "../funcs";
+import BillingItemRow from "./BillingItemRow/BillingItemRow";
+import PlusMinusButtons from "./PlusMinusButtons";
+import { RedButton } from "@/shared/components/Button";
+
+const BillingItems = ({ billingItems, setBillingItems }) => {
+  const [fuse, setFuse] = useState(null);
+
+  // Fetch products
+  useEffect(() => {
+    setBillingItems([createEmptyBillData()]);
+    fetchProducts(setFuse);
+  }, []);
+
+  return (
+    <div>
+      <Header>Billing Items</Header>
+      <div className="flex flex-col gap-2 mt-2">
+        {billingItems?.map((d, i) => (
+          <BillingItemRow
+            key={d.id}
+            rowData={d}
+            index={i}
+            billingItems={billingItems}
+            setBillingItems={setBillingItems}
+            fuse={fuse}
+          />
+        ))}
+      </div>
+      <PlusMinusButtons setBillingItems={setBillingItems} />
+      <RedButton
+        onClick={() => {
+          if (window.confirm("Do you want to clear?")) {
+            setBillingItems([]);
+            window.localStorage.removeItem("billingItems");
+          }
+        }}
+      >
+        Clear
+      </RedButton>
+    </div>
+  );
+};
+
+export default BillingItems;
