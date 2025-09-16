ea46e10a35cfab49286725feba49a637689f5376
Aadarsh Gupta <prathamtogupta11@gmail.com>
2025-09-16 21:10:52 +0530

    few printiable bill design improvement

--- src/features/Billing/PrintableBill/css.js
+++ src/features/Billing/PrintableBill/css.js
@@ -1,2 +1,2 @@
-export const srStyle = `w-[30px] border text-center px-1 py-2`;
-export const dataStyle = `min-w-[130px] border text-center px-2 py-3`;
+export const srStyle = `w-[10%] border text-center p-1 md:py-2`;
+export const dataStyle = `w-[30%] border text-center p-1 md:p-2`;
