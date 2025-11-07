ce62632c33ff2742421adbdad9b6e6f28b3135ec
Aadarsh Gupta <prathamtogupta11@gmail.com>
2025-11-07 16:35:45 +0530

    fixing loading screen

---
+++ src/app/go/products/loading.jsx
@@ -0,0 +1,8 @@
+import Server_Side_Loading from "@/re_usables/components/Loading/Server_Side_Loading";
+import React from "react";
+
+const loading = () => {
+  return <Server_Side_Loading message={`Products Loading...`} />;
+};
+
+export default loading;
